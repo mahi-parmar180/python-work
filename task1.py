ac9dbc79bafb523cdd761e00e0c150fcd45f7f5a
mahi-parmar180 <pmahi5836@gmail.com>
2026-01-13 09:21:08 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,44 @@
+# Simple Interest Calculation
+"""principal = float(input("Enter the principal amount: "))
+rate = float(input("Enter the rate of interest (in %): "))
+time = float(input("Enter the time (in years): "))
+simple_interest = (principal * rate * time) / 100
+print("Simple Interest is:", simple_interest)
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+if a > b:
+    print("Maximum number is:", a)
+else:
+    print("Maximum number is:", b)
+
+
+for i in range(1, 6):
+    print(i)"""
+
+# Using a variable
+my_string = "Hello, World!"
+print(len(my_string))
+
+name = "Mahi"
+print("Welcome,", name)
+
+text = "Mahi"
+print(text[0])
+
+text = "Mahi"
+print(text[-1])
+
+num = 10
+
+if num > 0:
+    print("This is a positive number")
+elif num < 0:
+    print("This is a negative number")
+else:
+    print("The number is zero")
+
+numbers = [10, 20, 30]
+print(sum(numbers))
+
+variable = input("Prompt message: ")
