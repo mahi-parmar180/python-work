402535cbff652513e4551b9a61a244283192cea1
mahi-parmar180 <pmahi5836@gmail.com>
2026-01-27 18:52:26 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,48 @@
+#Arithmetic operator
+a=5
+b=4
+print(a-b)
+print(a+b)
+print(a/b)
+
+#Assignment operator
+x=11
+print(x)
+
+#Unary minus
+a=4
+print(-a)
+
+#Relational operator
+p=10
+q=11
+print(p>q)
+print(p<q)
+
+#logical operator
+x=True
+y=False
+print(x and y)
+print(x or y)
+print(not x)
+
+#Boolean
+is_pass=True
+print(is_pass)
+
+#Bitwise operator
+a=5
+b=3
+print(a&b)
+print(a|b)
+
+#Membership operator
+number=[1,2,3,4]
+print(2 in number)
+print(4 not in number)
+
+#Identity operator
+x=10
+y=10
+print(x is y)
+print(x is not y)
