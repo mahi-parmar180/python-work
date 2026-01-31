88ecd98d75862ff6c7faf55338eb3c96ddf00331
mahi-parmar180 <pmahi5836@gmail.com>
2026-01-31 08:07:31 +0530

    Add files via upload

---
+++ io.py
@@ -0,0 +1,13 @@
+#output using print.
+print("Hello World!")
+
+#input using input.
+name=input("Enter your name")
+print("Hello",name)
+
+#input using input.
+city=input("Enter yuor city")
+print("Hello",city)
+
+#output using print.
+print("Hello Mahi")
